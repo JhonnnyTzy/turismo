ef6213ddde613503c42f6fd352864c000f0c4d8f
DevMaik <miguel.040.net@gmail.com>
2024-12-03 18:17:31 -0400

    contacosss

--- app/views/Menu/Contactos.php
+++ app/views/Menu/Contactos.php
@@ -1,93 +1,116 @@
-<h1>contactanos</h1>
 <div class="header_contactos">
+    <div class="contenido_contactos">
 
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor dolorem</h1>
+    </div>
 
 </div>
+<div class="contenedor_principal">
 
-<div class="formulario_contactos">
+    <div class="formulario_contactos">
 
-    <div class="informacion">
-        <h2>titulo</h2>
-        <p>parrafo</p>
+        <div class="informacion">
+            <h2>Titulo</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
 
-        <div class="contacto">
-            <div class="icono_contacto">
-                <p>icono</p>
+            <div class="contacto">
+                <div class="icono_contacto">
+                    <p></p>
+                </div>
+                <div class="informacion_contacto">
+                    <p class="titulo">titulo</p>
+                    <p class="departamento">departamento</p>
+                    <p>ciudad</p>
+                </div>
             </div>
-            <div class="informacion_contacto">
-                <p>titulo</p>
-                <p>departamento</p>
-                <p>ciudad</p>
+
+            <div class="contacto">
+                <div class="icono_contacto">
+                    <p></p>
+                </div>
+                <div class="informacion_contacto">
+                    <p class="titulo">titulo</p>
+                    <p class="departamento">departamento</p>
+                    <p>ciudad</p>
+                </div>
             </div>
+
+            <div class="contacto">
+                <div class="icono_contacto">
+                    <p></p>
+                </div>
+                <div class="informacion_contacto">
+                    <p class="titulo">titulo</p>
+                    <p class="departamento">departamento</p>
+                    <p>ciudad</p>
+                </div>
+            </div>
+            <hr>
+            <div class="redes_sociales">
+                <h2>Redes Sociales</h2>
+                <ul class="redes">
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </div>
+
         </div>
+        <div class="formulario">
+            <div class="formulario_content">
 
-        <div class="contacto">
-            <div class="icono_contacto">
-                <p>icono</p>
+                <h2>Enviar mensaje</h2>
+                <form action="" method="post" class="form_contacto">
+                    <div class="fila">
+                        <div class="box_content">
+                            <label for="">Nombre</label>
+                            <input type="text" placeholder="Nombre" class="box_form">
+                        </div>
+                        <div class="box_content">
+                            <label for="">Apellido</label>
+                            <input type="text" placeholder="Apellido" class="box_form">
+                        </div>
+                    </div>
+                    <div class="fila">
+                        <div class="box_content">
+                            <label for="">Telefono</label>
+                            <input type="text" placeholder="Telefono" class="box_form">
+                        </div>
+                        <div class="box_content">
+                            <label for="">Email</label>
+                            <input type="text" placeholder="Apellido" class="box_form">
+                        </div>
+                    </div>
+
+                    <div class="fila">
+                        <div class="box_content">
+                            <label for="">Observaciones</label>
+                            <input type="text" placeholder="Observaciones" class="box_form">
+                        </div>
+                    </div>
+                    <div class="fila">
+                        <div class="box_content">
+                            <label for="">Mensaje</label>
+                            <textarea name="" id="" rows="3" class="box_form"></textarea>
+                        </div>
+                    </div>
+                    <div class="fila">
+                        <div class="box_content">
+                            <button type="submit" class="boton box_form">Enviar</button>
+                        </div>
+                    </div>
+
+                </form>
             </div>
-            <div class="informacion_contacto">
-                <p>titulo</p>
-                <p>departamento</p>
-                <p>ciudad</p>
-            </div>
+
         </div>
-
-        <div class="contacto">
-            <div class="icono_contacto">
-                <p>icono</p>
-            </div>
-            <div class="informacion_contacto">
-                <p>titulo</p>
-                <p>departamento</p>
-                <p>ciudad</p>
-            </div>
-        </div>
-
-        <div class="redes_sociales">
-            <ul>
-                <li><a href="">Facebook</a></li>
-                <li><a href="">Instagram</a></li>
-                <li><a href="">X</a></li>
-                <li><a href="">Youtube</a></li>
-            </ul>
-        </div>
-
-    </div>
-    <div class="formulario">
-        <form action="" method="post">
-            <div class="row">
-                <div class="col-6">
-                    <label for="">Nombre</label>
-                    <input type="text" placeholder="Nombre">
-                </div>
-                <div class="col-6">
-                    <label for="">Apellido</label>
-                    <input type="text" placeholder="Apellido">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <label for="">Telefono</label>
-                    <input type="text" placeholder="Telefono">
-                </div>
-                <div class="col-6">
-                    <label for="">Email</label>
-                    <input type="text" placeholder="Apellido">
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-12">
-                    <label for="">Observaciones</label>
-                    <input type="text" placeholder="Observaciones">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <label for="">mensaje</label>
-                    <input type="text" placeholder="Mesanje">
-                </div>
-            </div>
-        </form>
     </div>
 </div>
+<div class="mapa">
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15301.404565416302!2d-68.13028888261718!3d-16.5083611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915f20638ea05dfd%3A0xef9b5857044c713a!2sFacultad%20de%20Tecnolog%C3%ADa!5e0!3m2!1ses-419!2sbo!4v1733263952246!5m2!1ses-419!2sbo" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+<?php  include_once 'app/views/partials/footer.php';?>
