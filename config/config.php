dd6b3843304b223f1374de76524dcc9ef5bcca4c
JhonnnyTzy <168737808+JhonnnyTzy@users.noreply.github.com>
2025-05-13 22:29:39 -0400

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 define("CONTROLADOR_DEFECTO", "Usuarios");
 define("ACCION_DEFECTO", "index");
 
-// configuración de la base de datos
+// Configuración de la base de datos
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'turismo');
 define('DB_USER', 'root');
@@ -11,8 +11,8 @@ define('DB_PASS', '');
 
 // Rutas base
 define("RUTA_BASE", $_SERVER['DOCUMENT_ROOT'] . "/");
-define("HTTP_BASE", "http://localhost/turismo");
+define("HTTP_BASE", "http://" . $_SERVER['HTTP_HOST'] . "/turismo");
 define("ROOT_APP", $_SERVER['DOCUMENT_ROOT'] . "/turismo/app/");
-define("ROOT_DIR,", $_SERVER['DOCUMENT_ROOT'] . "/turismo/");
+define("ROOT_DIR", $_SERVER['DOCUMENT_ROOT'] . "/turismo/");
 define("URL_RESOURCES", HTTP_BASE . "/public/");
 define("URL_RESOURCES_ADM", HTTP_BASE . "/public/adminlte/");
